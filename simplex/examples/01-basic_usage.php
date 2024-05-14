af2952e692da60e9f96297ecb5fa4c7e54652259
Simon Leclair <114162016+sim01275@users.noreply.github.com>
2024-05-14 15:14:12 +0200

    improve

--- simplex/examples/01-basic_usage.php
+++ simplex/examples/01-basic_usage.php
@@ -1,16 +1,27 @@
-<?php /** @noinspection ForgottenDebugOutputInspection */
+<?php
 
 use Shuchkin\SimpleXLSX;
 
-ini_set('error_reporting', E_ALL);
+// Enable error reporting and display errors
+error_reporting(E_ALL);
 ini_set('display_errors', true);
 
+// Include the SimpleXLSX library
 require_once __DIR__.'/../src/SimpleXLSX.php';
 
-echo '<h1>Parse books.xslx</h1><pre>';
-if ($xlsx = SimpleXLSX::parse('books.xlsx')) {
+// Set the filename of the XLSX file to parse
+$file = 'books.xlsx';
+
+// Parse the XLSX file
+if ($xlsx = SimpleXLSX::parse($file)) {
+    // Display the rows of the parsed XLSX file
+    echo '<h1>Parse ' . basename($file) . '</h1><pre>';
     print_r($xlsx->rows());
+    echo '</pre>';
 } else {
+    // Display an error message if the XLSX file could not be parsed
+    echo '<h1>Error parsing ' . basename($file) . '</h1><pre>';
     echo SimpleXLSX::parseError();
+    echo '</pre>';
 }
-echo '<pre>';
+
